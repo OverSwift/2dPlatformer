3227373ac1f4c22d7bd53dff456c8b54c19d0318
Sergiy Loza <sergiy.loza@lemberg.co.uk>
2016-12-11 15:26:21 +0200

    Add new menu button background

---
+++ Assets/Editor/SpriteCombinerEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor (typeof(SpriteCombiner))]
+public class SpriteCombinerEditor : Editor {
+
+	void OnSceneGUI() {
+
+		SpriteCombiner sc = target as SpriteCombiner;
+
+		if (Handles.Button (sc.transform.position + Vector3.up * 5, Quaternion.identity, 1, 1, Handles.CircleCap)) {
+			sc.CombineChildren ();
+		}
+	}
+}
---
+++ Assets/Scripts/SpriteCombiner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer))]
+public class SpriteCombiner : MonoBehaviour {
+
+	public void CombineChildren() {
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		SpriteRenderer[] renderers = GetComponentsInChildren<SpriteRenderer> ();
+		print ("Child sprite renderers " + renderers.Length + " count");
+
+		float finalTextureW = 0f;
+		float finalTextureH = 0f;
+
+
+
+		for (int i = 0; i < renderers.Length; i++) {
+			Sprite sprite = renderers [i].sprite;
+
+			if (sprite == null) {
+				print ("Opps renderer without sprite");
+				continue;
+			}
+			Texture2D texture = sprite.texture;
+
+//			print ("Sprite bounds: " + sprite.bounds);
+
+			print ("Transform : " + renderers [i].transform.localPosition);
+			print ("Texture size w:" + texture.width + "h:" + texture.height);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
